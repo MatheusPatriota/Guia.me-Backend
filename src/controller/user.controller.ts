9244322a6a5b4aa1cf174a27c466ee430690b80c
MatheusPatriota <mpatriota55@gmail.com>
2023-03-19 01:53:05 -0300

    feature: user get one implemented

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -2,9 +2,9 @@ import { Request, Response } from "express";
 import { auth } from "../config/firebase.config";
 import { validateFirebaseToken } from "../utils/validateToken";
 
-const GetAllUsers = async (req: Request, res: Response, next: any) => {
+const GetAllUsers = async (request: Request, response: Response, next: any) => {
   try {
-    const token = req.headers.authorization;
+    const token = request.headers.authorization;
     if (!token) throw new Error("Authorization token not found");
 
     const isValidToken = await validateFirebaseToken(token);
@@ -14,16 +14,44 @@ const GetAllUsers = async (req: Request, res: Response, next: any) => {
     console.log("users", users);
     if (!isValidToken) throw new Error("Error in load all users");
 
-    return res.status(200).json({
-      message: "Esse é seu primeiro request",
+    return response.status(200).json({
+      message: "Request Successful",
       users: users.users,
     });
-  } catch (err) {
-    console.error(err);
-    return res.status(401).json({
+  } catch (error) {
+    console.error(error);
+    return response.status(401).json({
       message: "Unauthorized",
     });
   }
 };
 
-export { GetAllUsers };
+const GetOneUser = async (request: Request, response: Response) => {
+  try {
+    const userId = request.params.id;
+    console.log("userId", userId)
+
+    if (!userId) throw new Error("User id parameter not found");
+
+    const token = request.headers.authorization;
+    if (!token) throw new Error("Authorization token not found");
+
+    const isValidToken = await validateFirebaseToken(token);
+    if (!isValidToken) throw new Error("Invalid authorization token");
+
+    const user = await auth.getUser(userId);
+    console.log("user", user);
+
+    return response.status(200).json({
+      message: "Request Successful",
+      users: user,
+    });
+  } catch (error) {
+    console.error(error);
+    return response.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+};
+
+export { GetAllUsers, GetOneUser };
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,8 +1,9 @@
 import express from 'express';
-import { GetAllUsers } from '../controller/user.controller';
+import { GetAllUsers, GetOneUser } from '../controller/user.controller';
 
 const userRoutes = express.Router()
 
 userRoutes.get('/', GetAllUsers);
+userRoutes.get('/:id', GetOneUser);
 
 export {userRoutes}
