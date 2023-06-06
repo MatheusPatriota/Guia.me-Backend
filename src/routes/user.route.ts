5128741d2273ee7cedf3b612024b302dfe4606d0
MatheusPatriota <mpatriota55@gmail.com>
2023-06-06 13:02:51 -0300

    feat: adding update user data route

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { GetAllUsers, GetOneUser } from '../controller/user.controller';
+import { GetAllUsers, GetOneUser, UpdateUserInformation } from '../controller/user.controller';
 
 const userRoutes = express.Router()
 
 userRoutes.get('/', GetAllUsers);
 userRoutes.get('/:id', GetOneUser);
+userRoutes.put('/:id', UpdateUserInformation);
 
 export {userRoutes}
--- src/utils/validateToken.ts
+++ src/utils/validateToken.ts
@@ -2,8 +2,14 @@ import {  auth } from "../config/firebase.config";
 
 export const validateFirebaseToken = async (token: string): Promise<any>  => {
   const filteredToken = token ? token.split(' ')[1] : "";
+  // console.log("/----------------------/")
+  // console.log("token", filteredToken)
+  // console.log("/----------------------/")
   try {
     const decodedToken = await auth.verifyIdToken(filteredToken);
+    // console.log("/----------------------/")
+    // console.log("decodedToken", decodedToken)
+    // console.log("/----------------------/")
     return true;
   } catch (error: any) {
     console.log("error", error);
